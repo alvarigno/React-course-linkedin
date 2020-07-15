f951575121e81e2e2ab82cc4650912769133c544
alvarigno <alvaro.emparan@gmail.com>
2020-07-15 12:53:10 -0400

    Initial commit

---
+++ React-course-linkedin/Exercise Files/CH 3/03_02/start/src/App.js
@@ -0,0 +1,33 @@
+import React, {Component} from 'react';
+import Title from './Title/Title';
+import Background from './Background/Background';
+import Grid from './Grid/Grid';
+import Rocket from './Rocket/Rocket';
+import Smoke from './Rocket/Smoke';
+
+import facts from './NASA_facts.json';
+
+//styles
+import styles from './App.css';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      facts,
+    };
+  }
+  render() {
+    return (
+      <div className={styles.app}>
+        <Title />
+        <Background />
+        <Grid facts={this.state.facts}/>
+        <Rocket />
+        <Smoke />
+      </div>
+    )
+  }
+}
+
+export default App;
